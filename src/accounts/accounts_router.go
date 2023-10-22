3d76fe054d9250cb0eed2da58eb58009ff1d0542
Michał Posłuszny <mipo756@gmail.com>
2023-10-22 10:11:13 +0200

    started work on accoounts & trarnscations

--- src/accounts/accounts_router.go
+++ src/accounts/accounts_router.go
@@ -1,10 +1,9 @@
-package panel
+package accounts
 
 import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/m-posluszny/go-ynab/src/accounts"
 	"github.com/m-posluszny/go-ynab/src/auth"
 )
 
@@ -13,7 +12,7 @@ func SetRoutes(r *gin.Engine) {
 	userPanel.Use(auth.AuthRequired)
 	{
 		userPanel.GET("/", RenderPanel)
-		userPanel.GET("/accounts", accounts.RenderPanel)
+
 	}
 	r.GET("/", func(c *gin.Context) {
 		c.Redirect(http.StatusSeeOther, "/panel")
